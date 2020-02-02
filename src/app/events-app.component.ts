7cf64ee5e4c28a082068e286eb9b8d178e704525
Orin Walker <orin.walker@gmail.com>
2020-02-02 13:23:53 -0500

    Finished Getting Started With Angular Module

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-app',
+  template: `
+  <h2>Hello World</h2>
+  <img src='/assets/images/basic-shield.png' />
+  `,
+})
+export class EventsAppComponent {
+  title = 'angular-fundamentals';
+}
