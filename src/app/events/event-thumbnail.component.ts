32cb9782918757c8df6a763afb70b474f74b322c
Orin Walker <orin.walker@gmail.com>
2020-02-03 09:27:25 -0500

    Some styling changes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
+    .green { color: #00aa00 !important}
+    .bold { font-weight:bold }
     .thumbnail { min-height: 210px }
     .pad-left { margin-left: 5px; }
     .well div { color: #bbb }
@@ -16,5 +18,10 @@ export class EventThumbnailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return { green: isEarlyStart, bold: isEarlyStart };
   }
 }
