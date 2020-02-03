c17fa6fc0a37c074636e59378c0bc18e84483743
Orin Walker <orin.walker@gmail.com>
2020-02-03 08:54:26 -0500

    more work with the event list and event thumbnails

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
+    .thumbnail { min-height: 210px }
     .pad-left { margin-left: 5px; }
     .well div { color: #bbb }
     `]
@@ -11,22 +12,9 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class EventThumbnailComponent implements OnInit {
 
   @Input() event: any;
-  // @Output() eventClick = new EventEmitter();
-
-  someProperty: any = 'some value';
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  // logFoo() {
-  //   console.log('foo');
-  // }
-
-  // handleClickMe() {
-  //   console.log('handleClickMe called!');
-  //   console.log('now emit the click event to the partent class as eventClick')
-  //   this.eventClick.emit(this.event.name);
-  // }
 }
