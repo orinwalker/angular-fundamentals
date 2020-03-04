7e694a07c20d3f3fb1039d27109fe41fc9a71456
Orin Walker <orin.walker@gmail.com>
2020-03-04 13:45:32 -0500

    added linter and made create event with 2-way binding (exercise)

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -10,7 +10,7 @@ import { EventService } from './shared';
 export class CreateEventComponent implements OnInit {
 
   isDirty: boolean = true;
-  newEvent;
+  newEvent: any;
 
   constructor(
     @Inject(forwardRef(() => Router)) private router: Router,
@@ -19,6 +19,32 @@ export class CreateEventComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.newEvent = {
+      // name: 'Ng Spectacular',
+      // date: '8/8/2028',
+      // time: '10am',
+      // price: 799.99,
+      // location: {
+      //   address: '456 Happy St',
+      //   city: 'Felicity',
+      //   country: 'Angularistan'
+      // },
+      // onlineUrl: 'http://ngSpectacular.com',
+      // imageUrl: 'http://ngSpectacular.com/logo.png'
+
+      name: '',
+      date: '',
+      time: '',
+      price: 0.00,
+      location: {
+        address: '',
+        city: '',
+        country: ''
+      },
+      onlineUrl: '',
+      imageUrl: ''
+
+    }
   }
 
   saveEvent(formValues) {
