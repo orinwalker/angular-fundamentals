e79deca085688a31de1ade1f0382b4b89457c092
Orin Walker <orin.walker@gmail.com>
2020-03-10 09:50:55 -0400

    added some unit tests for the voter service - post method

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -4,6 +4,8 @@ import { ISession } from '../shared';
 import { of } from 'rxjs';
 // import { Pipe } from '@angular/core';
 // import { EventsAppComponent } from './app/events-app.component';
+
+// tslint:disable: no-inferrable-types
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,39 +18,52 @@ describe('AppComponent', () => {
 
   let voterService: VoterService;
   let mockHttp: any;
+  let eventId: number;
+  let sessionId: number;
+  let voterName: string;
 
   beforeEach(async(() => {
     mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
     voterService = new VoterService(mockHttp);
+
   }));
 
   describe('deleteVoter', () => {
     it('should remove the voter from the list of voters', () => {
-      const session = { id: 6, voters: ['joe', 'john']};
+      eventId = 3; sessionId = 6; voterName = 'joe';
+      const session = { id: sessionId, voters: ['joe', 'john']};
       mockHttp.delete.and.returnValue(of(false));
-      console.log(session);
-      voterService.deleteVoter(3, session as ISession, 'joe');
+      voterService.deleteVoter(eventId, session as ISession, voterName);
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('john');
+    });
+    it('should call http.delete with the right URL', () => {
+      eventId = 3; sessionId = 6; voterName = 'joe';
+      const session = { id: sessionId, voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false));
+      voterService.deleteVoter(eventId, session as ISession, voterName);
+      const url = `/api/events/${eventId}/sessions/${sessionId}/voters/${voterName}`;
+      expect(mockHttp.delete).toHaveBeenCalledWith(url);
     });
   });
 
   describe('addVoter', () => {
     it('should add the voter to the list of voters', () => {
-      const session = { id: 6, voters: ['joe', 'john']};
+      eventId = 3; sessionId = 6; voterName = 'orin';
+      const session = { id: sessionId, voters: ['joe', 'john']};
       mockHttp.post.and.returnValue(of(true));
-      console.log(session);
-      voterService.addVoter(3, session as ISession, 'orin');
+      voterService.addVoter(eventId, session as ISession, voterName);
       expect(session.voters.length).toBe(3);
-      expect(session.voters[2]).toBe('orin');
+      expect(session.voters[2]).toBe(voterName);
+    });
+    it('should call http.post with the right URL', () => {
+      eventId = 3; sessionId = 6; voterName = 'joe';
+      const session = { id: sessionId, voters: ['john']};
+      mockHttp.post.and.returnValue(of(true));
+      voterService.addVoter(eventId, session as ISession, voterName);
+      const url = `/api/events/${eventId}/sessions/${sessionId}/voters/${voterName}`;
+      expect(mockHttp.post).toHaveBeenCalledWith(url, {}, jasmine.any(Object));
+      expect(session.voters.length).toBe(2);
     });
   });
-
-  // it('VoterService', () => {
-
-  //   expect(true).toBe(true);
-  // });
-  // it('should be false', () => {
-  //   expect(false).toBe(false);
-  // });
 });
